3711dff43606feb42c8922fe9d02f022dbdbc04c
jong0705 <jong0705q@gmail.com>
2025-07-28 23:42:33 +0900

    메타데이터 넣는중

---
+++ backend/daoData.js
@@ -0,0 +1,18 @@
+// backend/daoData.js
+const daoList = [
+  {
+    id: "DAO_TEST1",
+    name: "DAO_TEST1_NAME",
+    handle: "@dao_test1",
+    created: "2025.07",
+    member: 10,
+    vote: 20,
+    proposal: 35,
+    treasury: 1500000,
+    glassScore: 70,
+    voteParticipation: 60,
+  },
+  // ... 다른 DAO들
+];
+
+module.exports = daoList;
